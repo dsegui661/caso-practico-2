5e855ceb693b8996473ebb08dbc890772b8cda6a
David Seguí <fageca@gmail.com>
2023-03-03 19:58:56 +0100

    Ficheros parte 2 para despliege en AKS

--- terraform/output.tf
+++ terraform/output.tf
@@ -32,14 +32,13 @@ output "azurerm_public_ip" {
   description = "The actual ip address allocated for the resource."
   value       = azurerm_public_ip.public_ip.ip_address
 }
-# output "public_ip_dns_name" {
-#  description = "fqdn to connect to the first vm provisioned."
-#  value       = azurerm_public_ip.vm[*].fqdn
-# }
+
 output "network_interface_private_ip" {
   description = "private ip addresses of the vm nics"
   value       = azurerm_network_interface.ani01.private_ip_address
 }
+
+# Container registry
 output "admin_username" {
   description = "The Username associated with the Container Registry Admin account - if the admin account is enabled."
   value       = azurerm_container_registry.acr.admin_username
